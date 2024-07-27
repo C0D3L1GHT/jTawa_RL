4b9765bbd740cd3b3964d6aa149244c7952cb19e
Eric Reed <septem216@gmail.com>
2024-07-26 23:06:16 -0400

    #10 > I

--- components/fighter.py
+++ components/fighter.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 from components.base_component import BaseComponent
-from input_handlers import GameOverEventHandler
 from render_order import RenderOrder
 import color
 
@@ -30,7 +29,6 @@ class Fighter(BaseComponent):
         if self.engine.player is self.parent:
             death_message = "sina moli!"
             death_message_color = color.player_die
-            self.engine.event_handler = GameOverEventHandler(self.engine)
         else:
             death_message = f"{self.parent.name} li moli!"
             death_message_color = color.enemy_die
